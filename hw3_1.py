0d1a3184a06448b11960121d0e679703dd005658
Yury <arborsb158@gmail.com>
2020-12-02 19:58:08 +0300

    1

--- hw3_1.py
+++ hw3_1.py
@@ -2,5 +2,6 @@ new_list = list(input('Введите два числа через пробел:
 def new_div(arg_1, arg_2):
     return arg_1 / arg_2
 
-print(new_div(arg_1=int(new_list[0]), arg_2=int(new_list[1])))
-
+if int(new_list[0]) != 0 and int(new_list[1]) != 0:
+    print(new_div(arg_1=int(new_list[0]), arg_2=int(new_list[1])))
+else: print('0')
